5f9d651693ad362f45dea3759650b6fb83a6d3f3
Jian Qiu <jqiu@redhat.com>
2025-03-03 16:24:38 +0800

    test script for actas
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

---
+++ actas-test/test.sh
@@ -0,0 +1,20 @@
+
+echo "setup an actas sa and a user's permission"
+
+kubectl apply -f .
+
+echo "get token from sa and set the config"
+
+TOKEN=$(kubectl create token actas -n default)
+
+echo "sa actas user to get node will fail since sa does not have the permission"
+
+curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/nodes
+
+echo "sa actas user to get service will fail since the user does not have the permission"
+
+curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/services
+
+echo "sa actas user to get pods succeeds since both have the permission"
+
+curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/pods
