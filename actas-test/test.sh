795b833b2a575e749284a13cc24f07d73fb87bb3
Jian Qiu <jqiu@redhat.com>
2025-03-17 14:37:34 +0800

    Impersonate on
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- actas-test/test.sh
+++ actas-test/test.sh
@@ -9,12 +9,12 @@ TOKEN=$(kubectl create token actas -n default)
 
 echo "sa actas user to get node will fail since sa does not have the permission"
 
-curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/nodes
+curl -H"Authorization:Bearer $TOKEN" -H"Impersonate-User:bob" -k https://localhost:6443/api/v1/nodes
 
 echo "sa actas user to get service will fail since the user does not have the permission"
 
-curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/services
+curl -H"Authorization:Bearer $TOKEN" -H"Impersonate-User:bob" -k https://localhost:6443/api/v1/services
 
 echo "sa actas user to get pods succeeds since both have the permission"
 
-curl -H"Authorization:Bearer $TOKEN" -H"ActAs-User:bob" -k https://localhost:6443/api/v1/pods
+curl -H"Authorization:Bearer $TOKEN" -H"Impersonate-User:bob" -k https://localhost:6443/api/v1/pods
